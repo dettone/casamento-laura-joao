040aec95663f3c8ba1b80f8b2407e658715c9038
joaodettoni <joao.dettoni@maeztra.com>
2024-04-17 22:50:35 -0300

    make sql more  with sql injection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,26 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  res.status(200).json({ status: "OK" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  
+  const postgreesVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const databaseOpenedConnectionsResults = await database.query({text : "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+  values: [databaseName]});
+
+  
+  res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+          postgrees: {
+              version: postgreesVersion.rows[0].server_version,
+              max_connections: parseInt(maxConnections.rows[0].max_connections),
+              opened_connections: databaseOpenedConnectionsResults.rows[0].count,
+          },
+      },
+  });
+  
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+  
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt)
+  expect(responseBody.dependencies.postgrees.max_connections).toBeDefined()
+  expect(responseBody.dependencies.postgrees.opened_connections).toEqual(1)
+  expect(responseBody.dependencies.postgrees.version).toBeDefined()  
 });
+
+
